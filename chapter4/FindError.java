3b7b76287eb746aa9a1f09257b180d07070cf63d
Pankyes <praisemandik8@gmail.com>
2026-04-23 07:22:34 +0100

    chapter 4

---
+++ chapter4/FindError.java
@@ -0,0 +1,13 @@
+public class FindError{
+public static void main(String[]args){
+
+    if(age >= 65);
+        System.out.println("Age is greater than or equal to 65");
+    else
+        System.out.println("Age is less than 65");
+
+    }
+
+}
+
+//FindError.java:6: error: 'else' without 'if'
---
+++ chapter4/exercise416.java
@@ -0,0 +1,14 @@
+// Exercise 4.16: Mystery.java
+public class Mystery {
+public static void main(String[] args) {
+int x = -2;
+int total = 0;
+while (x <= 10) {
+int y = x + 2;
+x++;
+total += y;
+System.out.printf("Y is: %d and total is %d\n", y, total);
+ }// end while
+
+ }// end main
+ } // end class Mystery
